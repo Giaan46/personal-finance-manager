01395ce187c07d58d4d5c828bf1142729d7abd6d
Giaan46 <gianlucamaida996@gmail.com>
2026-03-04 21:19:48 +0100

    rename package

--- gianlu/src/main/java/com/gianlu/finance/gianlu/GianluApplication.java
+++ gianlu/src/main/java/com/gianlu/finance/gianlu/GianluApplication.java
@@ -1,4 +1,4 @@
-package com.gianlu.finance.application;
+package com.gianlu.finance.gianlu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
