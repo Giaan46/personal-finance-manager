56566faba4fd1287e01e2b94b6325e5af3650d6e
Giaan46 <gianlucamaida996@gmail.com>
2026-03-04 21:07:39 +0100

    initial spring boot project setup

---
+++ gianlu/src/main/java/com/gianlu/finance/gianlu/GianluApplication.java
@@ -0,0 +1,13 @@
+package com.gianlu.finance.gianlu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GianluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GianluApplication.class, args);
+	}
+
+}
---
+++ gianlu/src/test/java/com/gianlu/finance/gianlu/GianluApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gianlu.finance.gianlu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GianluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
